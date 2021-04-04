df8aa6742ab4195f19b96138ebcfb1f58c818109
Chris Hannaby <chris.hannaby@ubisense.net>
2021-04-04 15:12:41 -0600

    send emails via sendgrid

--- functions/handle-payment-succeeded.js
+++ functions/handle-payment-succeeded.js
@@ -8,11 +8,54 @@ const {
   STRIPE_WEBHOOK_SECRET,
   TWILIO_ACCOUNT_SID,
   TWILIO_AUTH_TOKEN,
-  TWILIO_NUMBER
+  TWILIO_NUMBER,
+  SENDGRID_API_KEY,
+  SENDGRID_EMAIL
 } = process.env
 
 const stripe = require("stripe")(STRIPE_SECRET_KEY);
 const twilio = require('twilio')(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN);
+const sendgrid = require('@sendgrid/mail');
+sendgrid.setApiKey(SENDGRID_API_KEY)
+
+const msg = {
+  to: 'chrishannaby@gmail.com',
+  from: SENDGRID_EMAIL,
+  templateId: 'd-359a4929e63d43e3aad92a53231cb5e9',
+  dynamicTemplateData: {
+    orderNumber: 1231231,
+    items: [
+      { name: "Barbell", price: "16.00" },
+      { name: "Dumbell", price: "6.00" }
+    ],
+    total: "22.00"
+
+  }
+}
+
+async function sendEmail() {
+  try {
+    await sendgrid.send(msg)
+    console.log("email sent")
+  }
+  catch (error) {
+    console.error(error)
+  }
+}
+
+async function sendSMS(customerPhone) {
+  try {
+    const response = await twilio.messages.create({
+      from: TWILIO_NUMBER,
+      to: customerPhone,
+      body: "🛒 Your Life Fitness order has been created!"
+    });
+    console.log("SMS sent", response)
+  }
+  catch (error) {
+    console.error('error sending SMS:', error)
+  }
+}
 
 // Create your webhook in the Stripe dashboard at https://dashboard.stripe.com/webhooks
 // Use the secret listed in the "Signing secret" section
@@ -40,20 +83,12 @@ exports.handler = async (event, context) => {
         "Payment was successful! Charge information:",
         paymentIntent.charges.data.filter(charge => charge.status === "succeeded")
       );
+      const tasks = [sendEmail()]
       const customerPhone = paymentIntent.metadata.phone
       if (customerPhone) {
-        try {
-          const response = await twilio.messages.create({
-            from: TWILIO_NUMBER,
-            to: customerPhone,
-            body: "order created!"
-          });
-          console.log("SMS sent", response)
-        }
-        catch (error) {
-          console.error('error sending SMS:', error)
-        }
+        tasks.push(sendSMS(customerPhone))
       }
+      await Promise.all(tasks)
       break;
     case "charge.dispute.created":
       const charge = stripeEvent.data.object;
