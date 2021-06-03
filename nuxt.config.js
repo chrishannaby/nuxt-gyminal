71171bc205d6767bb5c793f1251705f0579a9188
Chris Hannaby <chris.hannaby@netlify.com>
2021-06-03 07:53:22 -0600

    remove debug log statements

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,5 @@
 import cloudinary from "cloudinary";
 
-console.log("api_key", process.env.CLOUDINARY_API_KEY);
-
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
