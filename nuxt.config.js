089f2aa404cb83aa7aab13db27b470228196dfc7
Chris Hannaby <chris.hannaby@netlify.com>
2021-06-03 07:54:44 -0600

    update site title

--- nuxt.config.js
+++ nuxt.config.js
@@ -18,7 +18,7 @@ export default {
    ** Headers of the page
    */
   head: {
-    title: "Life Fitness",
+    title: "Gyminal",
     meta: [
       { charset: "utf-8" },
       { name: "viewport", content: "width=device-width, initial-scale=1" },
